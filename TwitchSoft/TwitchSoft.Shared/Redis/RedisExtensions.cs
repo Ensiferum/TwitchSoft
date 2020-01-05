6822c8beeab6138f833cc54576c790a50f7a26da
Siarhei Huliakevich <siarhei.huliakevich@logicsoftware.net>
2020-01-05 20:32:40 +0300

    Add redis helper

---
+++ TwitchSoft/TwitchSoft.Shared/Redis/RedisExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace TwitchSoft.Shared.Redis
+{
+    public static class RedisExtensions
+    {
+        public static void AddLocalRedis(this IServiceCollection services, IConfiguration configuration)
+        {
+            var redisHost = configuration.GetValue<string>("Redis:Host");
+            IConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisHost);
+            services.AddScoped(s => redis.GetDatabase());
+        }
+    }
+}
