994c8ebe17ac127727dab9777bbf957f4a72ec91
Siarhei Huliakevich <siarhei.huliakevich@logicsoftware.net>
2020-06-04 20:42:10 +0300

    Define kstrel listen port

--- TwitchSoft/TwitchSoft.TwitchBot/Program.cs
+++ TwitchSoft/TwitchSoft.TwitchBot/Program.cs
@@ -2,6 +2,7 @@
 using TwitchSoft.Shared.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace TwitchSoft.TwitchBot
 {
@@ -17,6 +18,12 @@ namespace TwitchSoft.TwitchBot
                 .ConfigureLogger()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        // Setup a HTTP/2 endpoint without TLS.
+                        options.ListenLocalhost(5000, o => o.Protocols =
+                            HttpProtocols.Http2);
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
