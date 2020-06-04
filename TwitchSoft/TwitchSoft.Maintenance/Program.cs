ffe9389b841475c9b043770f1f1d81daef7a889d
Siarhei Huliakevich <siarhei.huliakevich@logicsoftware.net>
2020-06-04 11:49:21 +0300

    use webhost for maintenance

--- TwitchSoft/TwitchSoft.Maintenance/Program.cs
+++ TwitchSoft/TwitchSoft.Maintenance/Program.cs
@@ -1,13 +1,7 @@
-using Coravel;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using TwitchSoft.Maintenance.Jobs;
-using TwitchSoft.Shared.Services.Models.Twitch;
-using TwitchSoft.Shared.Services.TwitchApi;
 using TwitchSoft.Shared.Logging;
-using TwitchSoft.Shared;
-using Microsoft.Extensions.Logging;
-using Coravel.Scheduling.Schedule.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
 
 namespace TwitchSoft.Maintenance
 {
@@ -15,56 +9,15 @@ namespace TwitchSoft.Maintenance
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            host.Services.UseScheduler(scheduler =>
-            {
-                //scheduler
-                //    .Schedule<OldMessagesCleaner>()
-                //    .Hourly();
-
-                scheduler
-                    .Schedule<HoneymadFollowsJoin>()
-                    .EveryFifteenMinutes();
-
-                scheduler
-                    .Schedule<EnsthorFollowsJoin>()
-                    .EveryFifteenMinutes();
-
-                scheduler
-                    .Schedule<ChannelsRefresher>()
-                    .EveryFifteenMinutes();
-
-                scheduler
-                    .Schedule<SentDailyMessageDigest>()
-                    .Cron("00 7,13,19 * * *");
-            }).LogScheduledTaskProgress(host.Services.GetService<ILogger<IScheduler>>());
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureLogger()
-                .ConfigureServices((hostContext, services) =>
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    services.ConfigureShared();
-                    // Set up the objects we need to get to configuration settings
-                    var Configuration = hostContext.Configuration;
-
-                    services.AddScoped<ITwitchApiService, TwitchApiService>();
-
-                    services
-                        .Configure<BotSettings>(Configuration.GetSection($"Twitch:{nameof(BotSettings)}"))
-                        .AddOptions();
-
-                    services.AddScheduler();
-
-                    //services.AddTransient<OldMessagesCleaner>();
-                    services.AddTransient<HoneymadFollowsJoin>();
-                    services.AddTransient<EnsthorFollowsJoin>();
-                    //services.AddTransient<TopChannelsJoin>();
-                    services.AddTransient<SentDailyMessageDigest>();
-                    services.AddTransient<ChannelsRefresher>();
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
---
+++ TwitchSoft/TwitchSoft.Maintenance/Startup.cs
@@ -0,0 +1,72 @@
+﻿using Coravel;
+using Microsoft.Extensions.DependencyInjection;
+using TwitchSoft.Maintenance.Jobs;
+using TwitchSoft.Shared.Services.Models.Twitch;
+using TwitchSoft.Shared.Services.TwitchApi;
+using TwitchSoft.Shared;
+using Microsoft.Extensions.Logging;
+using Coravel.Scheduling.Schedule.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace TwitchSoft.Maintenance
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureShared();
+
+            services.AddScoped<ITwitchApiService, TwitchApiService>();
+
+            services
+                .Configure<BotSettings>(Configuration.GetSection($"Twitch:{nameof(BotSettings)}"))
+                .AddOptions();
+
+            services.AddScheduler();
+
+            //services.AddTransient<OldMessagesCleaner>();
+            services.AddTransient<HoneymadFollowsJoin>();
+            services.AddTransient<EnsthorFollowsJoin>();
+            //services.AddTransient<TopChannelsJoin>();
+            services.AddTransient<SentDailyMessageDigest>();
+            services.AddTransient<ChannelsRefresher>();
+
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.ApplicationServices.UseScheduler(scheduler =>
+            {
+                //scheduler
+                //    .Schedule<OldMessagesCleaner>()
+                //    .Hourly();
+
+                scheduler
+                    .Schedule<HoneymadFollowsJoin>()
+                    .EveryFifteenMinutes();
+
+                scheduler
+                    .Schedule<EnsthorFollowsJoin>()
+                    .EveryFifteenMinutes();
+
+                scheduler
+                    .Schedule<ChannelsRefresher>()
+                    .EveryFifteenMinutes();
+
+                scheduler
+                    .Schedule<SentDailyMessageDigest>()
+                    .Cron("00 7,13,19 * * *");
+            }).LogScheduledTaskProgress(app.ApplicationServices.GetService<ILogger<IScheduler>>());
+        }
+    }
+}
