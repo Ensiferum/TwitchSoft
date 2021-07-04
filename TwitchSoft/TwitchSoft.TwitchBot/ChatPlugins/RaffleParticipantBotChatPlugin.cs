b70e419cab6cfaeeaac5ee68689b6d806955abcb
Siarhei Huliakevich <siarhei.huliakevich@logicsoftware.net>
2021-07-04 20:01:37 +0300

    decrease chance

--- TwitchSoft/TwitchSoft.TwitchBot/ChatPlugins/RaffleParticipantBotChatPlugin.cs
+++ TwitchSoft/TwitchSoft.TwitchBot/ChatPlugins/RaffleParticipantBotChatPlugin.cs
@@ -13,7 +13,7 @@ namespace TwitchSoft.TwitchBot.ChatPlugins
     public class RaffleParticipantBotChatPlugin : IChatPlugin
     {
         private const int NumberOfOccurencesToTrigger = 10;
-        private const int RandomizerChance = 40;
+        private const int RandomizerChance = 25;
         private readonly ILogger<RaffleParticipantBotChatPlugin> logger;
         private readonly IRecentCommandsCache recentCommandsCache;
         private readonly string[] ignoredCommands;
